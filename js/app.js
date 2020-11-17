5a7c40ae408c5e272c680dca1d8aba72be582abf
ParkDajeong <sobeast980@gmail.com>
2020-11-17 12:37:57 +0900

    Intro 텍스트 애니메이션 구현

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function() {
+  const text = "WE ARE FRIENDS!".split("");
+  let idx = 0;
+
+
+  // console.log(text);
+  // // $(".intro__text h2").text("");
+  // const timer = setInterval(function() {
+  //   if(idx < text.length) {
+  //     $(".intro__text h2").append("<span>" + text[idx++] + "</span>");
+  //     $(".intro__text h2 span:last").animate({opacity: 1}, 1000);
+  //   }else {
+  //     clearInterval(timer);
+  //   }
+  // }, 250);
+});
